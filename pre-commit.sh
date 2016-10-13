7f822fc9c0148ab3717efaa2b86e5d2a74c28e41
sway <swaylq0913@gmail.com>
2016-10-13 14:50:33 +0800

    Change pre commit file mode

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,1 +1,20 @@
-./.git/hooks/pre-commit
+#!/bin/sh
+#
+# An example hook script to verify what is about to be committed.
+# Called by "git commit" with no arguments.  The hook should
+# exit with non-zero status after issuing an appropriate message if
+# it wants to stop the commit.
+#
+# To enable this hook, rename this file to "pre-commit".
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo "Before your committing, lint your codes firstly.";
+npm run lint;
+if [ "$?" != "0" ]; then
+  echo;
+  echo "${RED}[Error]${NC} There are some problems in the project.";
+  echo "${RED}[Error]${NC} Fix them before committing or suggest a change to the rules defined in .eslintrc.";
+
+  exit 1;
+fi
